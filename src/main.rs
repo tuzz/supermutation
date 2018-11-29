5054c6fe9c9891a9687a70f708159edf5c5ee068
Chris Patuzzo <chris@patuzzo.co.uk>
2018-11-29 15:11:14 +0000

    Initialize N and FACTORIAL as static variables
    We’ll use these a lot so it makes sense to compute
    them once. N will eventually be provided by user
    input so it can’t be a constant.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+#![feature(uniform_paths)]
+
+#[macro_use]
+extern crate lazy_static;
+
 mod util;
+
+use util::Util;
+
+lazy_static! {
+    static ref N: usize = 5;
+    static ref FACTORIAL: usize = Util::factorial(*N);
+}
 
 fn main() {
     println!("Hello, world!");
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,9 +1,9 @@
-struct Util {
+pub struct Util {
 
 }
 
 impl Util {
-    fn factorial(n: usize) -> usize {
+    pub fn factorial(n: usize) -> usize {
         match n {
             1 => 1,
             _ => n * Self::factorial(n - 1),
